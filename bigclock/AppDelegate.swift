6e06e5e5dcdfd96a69d594cbe4965ae2b023bb7d
Adam Benzan <adam.benzan@gmail.com>
2015-12-04 15:54:43 -0500

    100% complete

---
+++ bigclock/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  bigclock
+//
+//  Created by Adam Benzan on 2015-10-01.
+//  Copyright © 2015 clawtros. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ bigclock/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  bigclock
+//
+//  Created by Adam Benzan on 2015-10-01.
+//  Copyright © 2015 clawtros. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    
+    var timer:NSTimer?
+    var formatter:NSDateFormatter = NSDateFormatter()
+    
+    @IBOutlet var backgroundView: NSView!
+    @IBOutlet weak var timerLabel: NSTextField!
+    
+    func updateTime() {
+        formatter.dateFormat = ""
+        formatter.timeStyle = .ShortStyle
+        timerLabel.stringValue = formatter.stringFromDate(NSDate())
+        timerLabel.sizeToFit()
+    }rn    func handleResize(event:AnyObject?) {
+        print(event)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(backgroundView)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "windowDidResize", name: "handleResize", object: nil)
+        backgroundView.wantsLayer = true
+        backgroundView.layer?.backgroundColor = CGColorCreateGenericRGB(0.2, 0.2, 0.2, 1)
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.5, target:self, selector: Selector("updateTime"), userInfo: nil, repeats: true)
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
